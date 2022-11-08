8abc64f323824a7b972de1161d585935d600b2d5
nghia <nghiaphamvan2002@gmail.com>
2022-11-08 09:14:21 +0700

    edit userfactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,11 +22,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => $email,
-            'type'=> 3,
+            'type' => 3,
             'email_verified_at' => now(),
             'avatar' => 'placeholder.png',
             'number_phone' => fake()->phoneNumber(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'type' =>'3',
             'remember_token' => Str::random(10),
         ];
     }
